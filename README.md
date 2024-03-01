7fcfad433f65ee36f2c90ce01bf1c5c2245971ae
afr2903 <afr102903@gmail.com>
2024-03-01 13:43:44 -0600

    Hand detection points w Mediapipe

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FrED_factory
+
+Collaborative Robotics Challenge with MIT for the Manufacturing Systems Automation class, group 602
+
---
+++ class_activities/OnlyHand.py
@@ -0,0 +1,42 @@
+import cv2
+import mediapipe as mp
+
+#Config camara
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+cap = cv2.VideoCapture(0)
+
+with mp_hands.Hands(
+    static_image_mode = False,
+    max_num_hands = 2,
+    min_detection_confidence = 0.7) as hands:
+    
+    while True:
+        ret, frame = cap.read()
+        #Obtain the dimension of the video
+        height, width, _ = frame.shape
+        if ret == False:
+            break
+            
+        height, width, _ = frame.shape
+        frame = cv2.flip(frame,1)
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(frame_rgb)
+        
+        if results.multi_hand_landmarks is not None:
+        
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawing.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS, 
+                       mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                       mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),)
+                
+                print(hand.landmark[mp_hands.HandLandmark.THUMB_TIP])
+        
+        
+        cv2.imshow("Frame", frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+            
+cap.release()
+cv2.destroyAllWindows()
