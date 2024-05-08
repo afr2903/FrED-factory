1d72714dce646afcc649d2e825b37b0fd748450b
afr2903 <afr102903@gmail.com>
2024-05-08 14:08:25 -0600

    feat: Draft for ESP teleop script (test missing)

---
+++ team2/gripper/teleop/teleop.ino
@@ -0,0 +1,86 @@
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <ESP32Servo.h>
+
+Servo small_gripper; // create servo object to control a servo
+Servo big_gripper;
+
+int small_pos = 0; // variable to store the servo position
+int big_pos = 0;
+
+const int SMALL_PWM = 13;
+const int BIG_PWM = 12;
+
+const char *ssid = "afr2903";
+const char *password = "12345678"; // Set password
+const int udpPort = 1234;
+
+WiFiUDP udp;
+
+IPAddress staticIP(192, 168, 18, 23); // Set the desired static IP address
+IPAddress gateway(192, 168, 18, 1);   // Set the gateway of your network
+IPAddress subnet(255, 255, 255, 0);   // Set the subnet mask of your network
+
+void setup(){
+    Serial.begin(115200);
+    Serial.println();
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED){
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+
+    Serial.println("Connected to WiFi");
+
+    WiFi.config(staticIP, gateway, subnet);
+
+    Serial.print("Current ip: ");
+    Serial.println(WiFi.localIP());
+
+    udp.begin(udpPort);
+    Serial.println("UDP server started");
+
+    ESP32PWM::allocateTimer(0);
+    ESP32PWM::allocateTimer(1);
+    ESP32PWM::allocateTimer(2);
+    ESP32PWM::allocateTimer(3);
+    small_gripper.setPeriodHertz(330);         // standard 50 hz servo
+    small_gripper.attach(smallPwm, 500, 2500); // attaches the servo on pin 18 to the servo object
+    big_gripper.setPeriodHertz(330);           // standard 50 hz servo
+    big_gripper.attach(bigPwm, 500, 2500);     // attaches the servo on pin 18 to the servo object
+}
+
+void loop(){
+    int packetSize = udp.parsePacket();
+    if (packetSize){
+        Serial.printf("Received %d bytes from %s, port %d\n", packetSize, udp.remoteIP().toString().c_str(), udp.remotePort());
+        char incomingPacket[255];
+        int len = udp.read(incomingPacket, 255);
+        bool big_requested = false;
+        if (len > 0){
+            if(incomingPacket[0] == 'b')
+                big_requested = true;
+
+            incomingPacket[len] = 0;
+            // Trim leading and trailing whitespaces
+            String message_completed;
+            for (int i = 1; i < len; i++){
+                char letter = incomingPacket[i];
+                if (letter != ' '){
+                    message_completed += letter;
+                    Serial.println(letter);
+                    Serial.println(message_completed);
+                }
+            }
+            int target_pos = message_completed.toInt();
+            Serial.printf("UDP packet contents: %i\n", target_pos);
+
+            if (big_requested){
+                big_gripper.write(target_pos);
+            } else {
+                small_gripper.write(target_pos);
+        }
+    }
+    delay(10);
+}
