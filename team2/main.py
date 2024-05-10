13e150483fea8d680c99b42f1177745b471d309d
Angma03 <maroal10@live.com>
2024-05-09 23:48:00 -0600

    shield positions

--- team2/main.py
+++ team2/main.py
@@ -18,17 +18,26 @@ class ElectronicsStation:
         "HOME": [85, 40],
         "PICK_ARDUINO": [20, 40],
         "PLACE_ARDUINO": [85, 40],
-        "PICK_WIRE": [85, 79]
+        "PICK_WIRE": [85, 79],
+        "PICK_SHIELD": [31, 40],
+        "PLACE_SHIELD":[85, 40]
     }
     ARM_STATES = {
         # [x, y, z, roll, pitch, yaw, speed]
-        "HOME": [0, -70, -20, 0, 90, 0, 30],
-        "BEFORE_PICK_ARDUINO": [84.4, -4.4, -17.2, -9.2, -19.4, 94.4, 30],
-        "PICK_ARDUINO": [11.4, 407.8, 164.2, -146.1, -0.8, 1.9, 20],
-        "AFTER_PICK_ARDUINO": [17.6, 349.4, 241, -139.1, 0.2, -0.8, 30],
-        "BEFORE_PLACE_ARDUINO": [59.8, 0.8, -23.6, -8.9, 24.6, 68.8, 30],
-        "PLACE_ARDUINO": [166.3, 287.4, 124.2, 176.8, 0.3, 1.5, 20],
-        "AFTER_PLACE_ARDUINO": [166.3, 287.4, 145, 176.8, 0.3, 1.5, 20],
+        "HOME": [0, -70, -20, 0, 90, 0, 30], #JOINT
+        "BEFORE_PICK_ARDUINO": [84.4, -4.4, -17.2, -9.2, -19.4, 94.4, 30], #JOINT
+        "PICK_ARDUINO": [11.4, 407.8, 164.2, -146.1, -0.8, 1.9, 20], #LINEAR
+        "AFTER_PICK_ARDUINO": [17.6, 349.4, 241, -139.1, 0.2, -0.8, 30], #LINEAR
+        "BEFORE_PLACE_ARDUINO": [59.8, 0.8, -23.6, -8.9, 24.6, 68.8, 30], #JOINT
+        "PLACE_ARDUINO": [166.3, 287.4, 124.2, 176.8, 0.3, 1.5, 20], #LINEAR
+        "AFTER_PLACE_ARDUINO": [166.3, 287.4, 145, 176.8, 0.3, 1.5, 20], #LINEAR
+        "BEFORE_PICK_SHIELD":[117.4, 0.6, -37.5, 90.1, -13.6, 21.3, 30], #JOINT
+        "PICK_SHIELD":[-94.3, 440.7, 183.9, -147.1, 0.9, -0.7, 20], #LINEAR
+        "AFTER_PICK_SHIELD":[-84.4, 372.6, 262.2, -141.1, -1.7, 4.1, 30], #LINEAR
+        "BEFORE_PLACE_SHIELD":[48.6, -16.9, -8, -0.4, 26.3, 230.3, 30], #JOINT
+        "PLACE_SHIELD":[166.3, 193.5, 130.1, 0.5, 179.4, -0.3, 20], #LINEAR
+        "AFTER_PLACE_SHIELD":[166.3, 192, 181.5, 0.2, 179.4, 0.7, 20] #LINEAR
+
     }
     def __init__(self):
         """Initialize communications"""
@@ -100,7 +109,36 @@ class ElectronicsStation:
 
         elif self.current_state == "AFTER_PLACE_ARDUINO":
             self.send_arm_state(self.current_state, lineal=True)
+            self.current_state = "BEFORE_PICK_SHIELD"
+
+        elif self.current_state == "BEFORE_PICK_SHIELD":
+            self.send_arm_state(self.current_state)
+            self.current_state = "PICK_SHIELD"
+
+        elif self.current_state == "PICK_SHIELD":
+            self.send_arm_state(self.current_state, lineal=True)
+            self.send_gripper_state(self.current_state)
+            time.sleep(1)
+            self.current_state = "AFTER_PICK_SHIELD"
+        
+        elif self.current_state == "AFTER_PICK_SHIELD":
+            self.send_arm_state(self.current_state, lineal=True)
+            self.current_state = "BEFORE_PLACE_SHIELD"
+
+        elif self.current_state == "BEFORE_PLACE_SHIELD":
+            self.send_arm_state(self.current_state)
+            self.current_state = "PLACE_SHIELD"
+
+        elif self.current_state == "PLACE_SHIELD":
+            self.send_arm_state(self.current_state, lineal=True)
+            self.send_gripper_state(self.current_state)
+            time.sleep(1)
+            self.current_state = "AFTER_PLACE_SHIELD"
+
+        elif self.current_state == "AFTER_PLACE_SHIELD":
+            self.send_arm_state(self.current_state, lineal=True)
             self.current_state = "HOME"
+
 
         time.sleep(0.1)
 
